5762e610844cac40e9ea3b0550e0f4e4ee899f36
Matej Stanković <mstankovic1@etfos.hr>
2026-03-11 13:15:05 +0100

    Add files via upload

---
+++ try.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib . pyplot as plt
+a = np. array ([6, 2, 9]) # napravi polje od tri elementa
+print ( type (a)) # prikazi tip polja
+print (a. shape )
+#print (a[0], a[1], a[2]) # prikazi prvi , drugi i treci element
+#print (a[1:2]) # izdvajanje
+#print (a[1:-1]) # izdvajanje
+#b = np. array ([[3,7,1],[4,5,6]]) # napravi 2 dimenzionalno polje ( matricu )
+#
+#print (b[0, 2], b[0, 1], b[1, 1]) # ispisi neke elemente polja
+#print (b[0:2,0:1]) # izdvajanje
+#print (b[:,0])
+#c = np. zeros ((4,2))
+#print(c)
+
+#a = np. array ([3,1,5], float )
+#b = np. array ([2,4,8], float )
+#print (a. min ())
+#print (a. argmin ())
+#print (a. max())
+#print (a. argmax ())
+#print (a. sum ())
+#print (a. mean ())
+#print (np. mean (a))
+#print (np. max (a))
+#print (np. sum (a))
+#a. sort ()
+#print (a)
+#import numpy as np
+#import matplotlib . pyplot as plt
+#img = plt . imread ("C:/Users/student/Desktop/LV2 Rad s bibliotekama Numpy i Matplotlib-20260311/road.jpg")
+#img = img [:,:,0]. copy ()
+#print ( img . shape )
+#print ( img . dtype )
+#plt . figure ()
+#plt . imshow (img , cmap ="gray")
+#plt . show ()
